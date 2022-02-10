019d7499356955f9d4c1054cf893b05d1ab1c956
GabrielGalindo1 <gabrieldelimagalindo.gdlg@gmail.com>
2022-02-10 18:43:55 -0300

    Neste desafio foram criados Classes e objetos e estruturas de repetição para a execução, tive um pouco de dificuldade, mas graças a ajuda de todos consegui
    Classe Aluno
    Objeto aluno
    
    For
    Foreach
    If

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { rawListeners } from "process";
+import { NOMEM } from "dns";
 
 dotenv.config();
 
@@ -32,12 +34,42 @@ app.use(express.json());
 /**
  * Server Activation
  */
-
-app.listen(PORT, () => {
+	class Aluno{
+		nome : string = '';
+		nota : number = 0;
+	}
+app.listen(PORT, async () => {
 	console.log(`Listening on port ${PORT}`);
 
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
 	
-});
+	const readline = require('readline-sync')
+
+	const qntdAluno = readline.question('Quantidade de Alunos?\n')
+	
+	let i = 0;
+	let listaDeAlunos: Aluno[] = [];
+
+	for(i= 0; i < qntdAluno; i++){
+		const aluno = new Aluno();
+		aluno.nome = readline.question('Nome do Aluno?\n');
+		aluno.nota = readline.question('Nota do Aluno?\n');
+		listaDeAlunos.push(aluno);
+		console.log(listaDeAlunos)
+	} 
+	let alunoMaiorNota: Aluno = new Aluno ;
+
+	listaDeAlunos.forEach(function(aluno){
+		if(typeof alunoMaiorNota == 'undefined'){
+		alunoMaiorNota= aluno;
+		}
+		if(aluno.nota > alunoMaiorNota.nota){
+			alunoMaiorNota= aluno;
+
+		}		
+	})
+	console.log('O Aluno com a maior nota e:',alunoMaiorNota)
+	
+})
